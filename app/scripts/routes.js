de2a4b430669e6fa6107545641d33e28fe049874
Lorenzo Paoliani <lorenzo.paoliani13@imperial.ac.uk>
2015-02-07 21:33:15 +0000

    updated routes

--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -37,88 +37,88 @@ angular.module('thankDonateApp')
  * dependency injection (see AccountCtrl), or rejects the promise if user is not logged in,
  * forcing a redirect to the /login page
  */
-  .config(['$routeProvider', 'SECURED_ROUTES', function($routeProvider, SECURED_ROUTES) {
+.config(['$routeProvider', 'SECURED_ROUTES', function($routeProvider, SECURED_ROUTES) {
     // credits for this idea: https://groups.google.com/forum/#!msg/angular/dPr9BpIZID0/MgWVluo_Tg8J
     // unfortunately, a decorator cannot be use here because they are not applied until after
     // the .config calls resolve, so they can't be used during route configuration, so we have
     // to hack it directly onto the $routeProvider object
     $routeProvider.whenAuthenticated = function(path, route) {
-      route.resolve = route.resolve || {};
-      route.resolve.user = ['authRequired', function(authRequired) {
-        return authRequired();
-      }];
-      $routeProvider.when(path, route);
-      SECURED_ROUTES[path] = true;
-      return $routeProvider;
+        route.resolve = route.resolve || {};
+        route.resolve.user = ['authRequired', function(authRequired) {
+            return authRequired();
+        }];
+        $routeProvider.when(path, route);
+        SECURED_ROUTES[path] = true;
+        return $routeProvider;
     };
-  }])
+}])
 
-  // configure views; the authRequired parameter is used for specifying pages
-  // which should only be available while logged in
-  .config(['$routeProvider', function($routeProvider) {
+// configure views; the authRequired parameter is used for specifying pages
+// which should only be available while logged in
+.config(['$routeProvider', function($routeProvider) {
     $routeProvider
-      .when('/', {
+    .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
-      })
+    })
 
-      .when('/login', {
+    .when('/login', {
         templateUrl: 'views/login.html',
         controller: 'LoginCtrl'
-      })
+    })
 
-      .when('/chat', {
+    .when('/chat', {
         templateUrl: 'views/chat.html',
         controller: 'ChatCtrl'
-      })
+    })
 
-      .whenAuthenticated('/account', {
+    .whenAuthenticated('/account', {
         templateUrl: 'views/account.html',
         controller: 'AccountCtrl'
-      })
-    
-      .whenAuthenticated('/create', {
+    })
+
+    .whenAuthenticated('/create', {
         templateUrl: 'views/create.html',
         controller: 'CreateCtrl'
-      })
+    })
 
-      .when('/chat', {
-        templateUrl: 'views/chat.html',
-        controller: 'ChatCtrl'
-      })
-      .otherwise({redirectTo: '/'});
-  }])
+    .when('/read/:articleID', {
+        templateUrl: 'views/read.html',
+        controller: 'ReadCtrl'
+    })
+    .otherwise({redirectTo: '/'});
+}])
 
-  /**
+/**
    * Apply some route security. Any route's resolve method can reject the promise with
    * { authRequired: true } to force a redirect. This method enforces that and also watches
    * for changes in auth status which might require us to navigate away from a path
    * that we can no longer view.
    */
-  .run(['$rootScope', '$location', 'simpleLogin', 'SECURED_ROUTES', 'loginRedirectPath',
-    function($rootScope, $location, simpleLogin, SECURED_ROUTES, loginRedirectPath) {
-      // watch for login status changes and redirect if appropriate
-      simpleLogin.watch(check, $rootScope);
+.run(['$rootScope', '$location', 'simpleLogin', 'SECURED_ROUTES', 'loginRedirectPath',
+      function($rootScope, $location, simpleLogin, SECURED_ROUTES, loginRedirectPath) {
+          // watch for login status changes and redirect if appropriate
+          simpleLogin.watch(check, $rootScope);
 
-      // some of our routes may reject resolve promises with the special {authRequired: true} error
-      // this redirects to the login page whenever that is encountered
-      $rootScope.$on('$routeChangeError', function(e, next, prev, err) {
-        if( angular.isObject(err) && err.authRequired ) {
-          $location.path(loginRedirectPath);
-        }
-      });
+          // some of our routes may reject resolve promises with the special {authRequired: true} error
+          // this redirects to the login page whenever that is encountered
+          $rootScope.$on('$routeChangeError', function(e, next, prev, err) {
+              if( angular.isObject(err) && err.authRequired ) {
+                  $location.path(loginRedirectPath);
+              }
+          });
 
-      function check(user) {
-        if( !user && authRequired($location.path()) ) {
-          $location.path(loginRedirectPath);
-        }
+          function check(user) {
+              if( !user && authRequired($location.path()) ) {
+                  $location.path(loginRedirectPath);
+              }
+          }
+
+          function authRequired(path) {
+              return SECURED_ROUTES.hasOwnProperty(path);
+          }
       }
+     ])
 
-      function authRequired(path) {
-        return SECURED_ROUTES.hasOwnProperty(path);
-      }
-    }
-  ])
-
-  // used by route security
-  .constant('SECURED_ROUTES', {});
+// used by route security
+.constant('SECURED_ROUTES', {});
