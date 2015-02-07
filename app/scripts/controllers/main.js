ba0161256f6662b7d1c7a5c306583fffb95b5e00
Lorenzo Paoliani <lorenzo.paoliani13@imperial.ac.uk>
2015-02-07 21:33:05 +0000

    Homepage

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,6 @@
  * Controller of the thankDonateApp
  */
 angular.module('thankDonateApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, ArticleRetriever) {
+    $scope.articles = ArticleRetriever.getAll();
   });
